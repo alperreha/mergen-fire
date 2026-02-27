64a0062e6d0676484369ac9f0c743e803b7b123a
Alper Reha Yazgan <alperreha@gmail.com>
2026-02-27 15:06:12 +0300

    fix: app.css import forgot fix

--- apps/dashboard/src/entry-client.tsx
+++ apps/dashboard/src/entry-client.tsx
@@ -1,4 +1,6 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
+import "@fontsource/noto-sans";
+import "./app.css";
 
-mount(() => <StartClient />, document.getElementById("app")!);
+mount(() => <StartClient />, document.getElementById("app"));
